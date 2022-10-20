8ad063c366af03f14dc4de47a207d8996b8f23d3
RunchenGeng <s2332611@ed.ac.uk>
2022-10-20 16:02:56 +0100

    final

--- dloop.R
+++ dloop.R
@@ -1,38 +1,39 @@
 dloop <- function(n, nrep) {
   final_result <- rep(0, 2 * n)
-  #used to store result
   for (i in 1:nrep) {
     result <- rep(0, 2 * n)
     u <- sample(1:(2 * n), 2 * n)
     check <- u
-    #this is for test, which has 1 one-loop, 2 two-loop, 1 three-loop and 1 four-loop
-    
     for (k in u) {
       loop_len <- 1
       initial <- k
-      #path <- c(k)
-      #we generate or say re-generate of start value of loop at start of each for-loop
-      #while (k != 0) {
-      #for check, not sure if we need to add this line
       while (u[k] != initial & u[k] != 0) {
         loop_len <- loop_len + 1
         k <- u[k]
-        #path <- c(path,which(u == u[k]))
-        
       }
-      #break
-      #}
-      #u[path] <- 0
       result[loop_len] <- 1
-      #our aim is to count whether the loop with index length occurred so i set it as one robustly
     }
-    #print(i)
     final_result <- final_result + result
   }
+  final_result/nrep
 }
-dloop(50, 10000)
 
 
+fin <- dloop(50, 10000)
+
+sum <- 0
+for (i in 51:100) {
+  
+  sum <- sum + fin[i]
+}
+prb <- 1-sum
 
 
+check <- 1/(1:100)
+sumcheck <- 0
+for (i in 51:100) {
+  
+  sumcheck <- sumcheck + check[i]
+}
+prbcheck <- 1-sumcheck
 
